cbad3abf183aca3d5d355b76101f47f5882a1937
Sebastian Baldini <sebastian.baldini@outlook.com>
2023-11-13 12:00:01 -0500

    added tests

--- face_detection.py
+++ face_detection.py
@@ -2,7 +2,7 @@
 
 import cv2 as cv
 
-img = cv.imread(r'Resources\Group_Photos\Retreat 23-24 Group\20231016210431-ba0e08a2-la.jpg')
+img = cv.imread(r'Students_in_Large_Classroom_with_Laptops.jpg')
 # cv.imshow('Group of 5 people', img)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -10,7 +10,7 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 haar_cascade = cv.CascadeClassifier('haar_face.xml')
 
-faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=8)
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=2)
 
 print(f'Number of faces found = {len(faces_rect)}')
 
