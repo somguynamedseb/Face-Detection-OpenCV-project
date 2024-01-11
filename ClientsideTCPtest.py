e4ac5bde8816e992097107f55c5863b711d191cc
Sebastian Baldini <sebastian.baldini@outlook.com>
2024-01-11 14:20:52 -0500

    updating IPS

--- ClientsideTCPtest.py
+++ ClientsideTCPtest.py
@@ -2,7 +2,7 @@
 
 import socket
 
-HOST = "192.168.1.34"  # The server's hostname or IP address
+HOST = "192.168.4.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
